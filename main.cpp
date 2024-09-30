52e58a64d4ad7e21c7e4c9ea409dbb87629aa11e
takuma <k023g0015@g.neec.ac.jp>
2024-09-30 12:30:10 +0900

    確認課題完了

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 ﻿#include <stdio.h>
 
+// テンプレート関数
+template <typename Type>
+Type Min(Type a, Type b) {
+	if (a < b) {
+		return static_cast<Type>(a);
+	} else if (a > b) {
+		return static_cast<Type>(b);
+	}
+}
+
+// char 型専用の関数
+template<>
+const char* Min(const char* a, const char* b) {
+	const char* point = "数字以外は代入できません";
+	return point;
+}
+
 int main() {
+	printf("%d\n", Min<int>(3, 9));
+	printf("%f\n", Min<float>(7.0f, 4.0f));
+	printf("%f\n", Min<double>(9.7, 9.8));
+	printf("%s\n", Min<const char*>("は", "ひ"));
 
 	return 0;
 }
